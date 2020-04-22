d2bd393434b1f6f7a635869bdf214119a0c558c1
Reza Karimi <reza_karimi76@yahoo.com>
2020-04-22 19:53:36 +0200

    Update eslintrc.js

--- eslintrc.js
+++ eslintrc.js
@@ -3,21 +3,36 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ["prettier"],
+  extends: [ "eslint:recommended" ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
   },
-  parserOptions: {
+  "parser": "babel-eslint",
+  parserOptions: { 
+    
     ecmaVersion: 2018, 
     sourceType: "module",
+    "allowImportExportEverywhere": true
   },
-  plugins: [
-    "prettier"
-  ],
   "rules": {
     "space-in-parens": ["error", "always"],
-    "quotes": ["error", "single"],
-    "prettier/prettier": "error"
+    "space-before-function-paren": ["error", "never"],
+    "keyword-spacing": ["error", { after: false }],
+    "space-in-brackets": ["error", "always"],
+    "object-curly-spacing": ["error", "always", { "objectsInObjects": true }],
+    "object-property-newline": ["error"],
+    "array-bracket-spacing": ["error", "always", { "arraysInArrays": true }],
+    "curly": ["error", "multi-or-nest"],
+    "brace-style": ["error", "allman", { "allowSingleLine": true }],
+    "nonblock-statement-body-position": ["error", "below"],
+    "arrow-parens": ["error", "as-needed"],
+    "quotes": ["error", "double"],
+    "indent": ["error", 2],
+    "no-multi-spaces": "error",
+    "no-multiple-empty-lines": ["error", { "max": 1 }],
+    "operator-linebreak": ["error", "none"],
+    "linebreak-style": ["error", "unix"],
+    "comma-dangle": ["error", "never"],
   }  
 };
